3f0f3a78b92c754b2da89c205277da9fcde56975
Juniper V. <pinoypasta@gmail.com>
2024-08-15 23:25:13 -0700

    Edit style and functions for better formatting, code readability, and functionality

--- drawingCode.js
+++ drawingCode.js
@@ -1,24 +1,32 @@
-let error = document.querySelector("#error");
-let pixelGrid = document.querySelector("#pixelGrid");
+/*
+Main Interface
+*/
 
+// User Input (Dimensions)
 let height = document.querySelector("#height");
 let width = document.querySelector("#width");
 
+// Making the Pixel Canvas
+let error = document.querySelector("#error");
 let newSketch = document.querySelector("#newSketch");
+let pixelGrid = document.querySelector("#pixelGrid");
 
+/*
+Creates Pixel Canvas
+*/
 function adjustGrid() {
-    /*
-    This function fills out the pixel grid with pixels based on the inputted dimensions.
-    */
+    // User Input (Dimensions)
     let y = document.querySelector("#height").value;
     let x = document.querySelector("#width").value;
 
-    if (x > 100 || y > 100) {
+    // Creates Canvas or Throws an Error
+    if (x > 100 || y > 100 || x < 0 || y < 0) {
         let errorText = document.createElement("p");
         errorText.textContent = "Cannot create canvas. Either the canvas is too big or the values aren't valid.";
         error.textContent = "";
         error.appendChild(errorText);
     } else {
+        error.textContent = "";
         pixelGrid.setAttribute("style", `height: ${y}rem; width:${x}rem`);
 
         i = x * y;
@@ -29,6 +37,9 @@ function adjustGrid() {
             pixel.addEventListener("click", () => {
                 pixel.setAttribute("class", "drawnPixel");
             });
+            pixel.addEventListener("dblclick", () => {
+                pixel.setAttribute("class", "pixel");
+            });
 
             pixelGrid.appendChild(pixel);
     
@@ -36,6 +47,10 @@ function adjustGrid() {
         }
     }
 }
+
+/* 
+Event Listener for "New Sketch!" Button
+*/
 
 newSketch.addEventListener("click", () => {
     pixelGrid.textContent = '';
